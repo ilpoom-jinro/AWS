1a9f851eb8c57669b43ec6d6688c597567a19877
zzo220 <dhyeon5347@gmail.com>
2026-05-15 16:41:42 +0900

    security: GitHub Actions IAM 권한 최소화 (iam:* 제거)

--- iam/github-oidc.tf
+++ iam/github-oidc.tf
@@ -34,8 +34,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          # 본인 레포 주소로 수정
-          "token.actions.githubusercontent.com:sub" =  "repo:ilpoom-jinro/AWS:*"
+          "token.actions.githubusercontent.com:sub" = "repo:ilpoom-jinro/AWS:*"
         }
       }
     }]
@@ -74,10 +73,59 @@ resource "aws_iam_role_policy" "github_actions_terraform" {
         ]
       },
       {
-        # IAM 리소스 관리 (사용자, 그룹, 정책, OIDC 등)
+        # IAM 리소스 관리 — iam:* 대신 실제 필요한 액션만 명시
         Sid    = "IAMManagement"
         Effect = "Allow"
-        Action = ["iam:*"]
+        Action = [
+          # 유저
+          "iam:CreateUser", "iam:DeleteUser", "iam:GetUser",
+          "iam:TagUser", "iam:UntagUser", "iam:ListUserTags", "iam:ListUsers",
+
+          # 그룹
+          "iam:CreateGroup", "iam:DeleteGroup", "iam:GetGroup", "iam:ListGroups",
+
+          # 그룹 멤버십
+          "iam:AddUserToGroup", "iam:RemoveUserFromGroup", "iam:ListGroupsForUser",
+
+          # 그룹 정책 연결
+          "iam:AttachGroupPolicy", "iam:DetachGroupPolicy", "iam:ListAttachedGroupPolicies",
+
+          # 관리형 정책
+          "iam:CreatePolicy", "iam:DeletePolicy",
+          "iam:GetPolicy", "iam:GetPolicyVersion",
+          "iam:CreatePolicyVersion", "iam:DeletePolicyVersion",
+          "iam:ListPolicies", "iam:ListPolicyVersions", "iam:ListEntitiesForPolicy",
+
+          # 유저 인라인 정책
+          "iam:PutUserPolicy", "iam:DeleteUserPolicy",
+          "iam:GetUserPolicy", "iam:ListUserPolicies",
+
+          # 액세스 키
+          "iam:CreateAccessKey", "iam:DeleteAccessKey",
+          "iam:ListAccessKeys", "iam:GetAccessKeyLastUsed",
+
+          # OIDC 프로바이더
+          "iam:CreateOpenIDConnectProvider", "iam:DeleteOpenIDConnectProvider",
+          "iam:GetOpenIDConnectProvider", "iam:ListOpenIDConnectProviders",
+          "iam:TagOpenIDConnectProvider", "iam:ListOpenIDConnectProviderTags",
+
+          # IAM 롤
+          "iam:CreateRole", "iam:DeleteRole", "iam:GetRole",
+          "iam:UpdateRole", "iam:UpdateAssumeRolePolicy",
+          "iam:TagRole", "iam:UntagRole", "iam:ListRoleTags", "iam:ListRoles",
+
+          # 롤 인라인 정책
+          "iam:PutRolePolicy", "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy", "iam:ListRolePolicies",
+
+          # 롤 관리형 정책 연결
+          "iam:AttachRolePolicy", "iam:DetachRolePolicy", "iam:ListAttachedRolePolicies",
+
+          # 계정 패스워드 정책
+          "iam:GetAccountPasswordPolicy",
+          "iam:UpdateAccountPasswordPolicy",
+          "iam:DeleteAccountPasswordPolicy"
+        ]
         Resource = "*"
       }
     ]
