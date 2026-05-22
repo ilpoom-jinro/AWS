01c7ae650535815e164c47a54130f6d28459ebb7
JunYoungLee260 <jylee5847@gmail.com>
2026-05-22 12:12:22 +0900

    chore : eks add-on 관련 ebs-csi creating 오류 수정 코드
    iam 권한 추가

--- vpc/globalservice/eks.tf
+++ vpc/globalservice/eks.tf
@@ -96,6 +96,33 @@ resource "aws_iam_role_policy_attachment" "eks_node_ebs_csi" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
+resource "aws_iam_role" "ebs_csi" {
+  name = "${var.eks_cluster_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "pods.eks.amazonaws.com"
+      }
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+    }]
+  })
+
+  tags = {
+    Name = "${var.eks_cluster_name}-ebs-csi-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
 resource "aws_eks_node_group" "service" {
   cluster_name    = aws_eks_cluster.service.name
   node_group_name = "${var.eks_cluster_name}-general"
@@ -159,11 +186,35 @@ resource "aws_eks_addon" "kube_proxy" {
   depends_on = [aws_eks_node_group.service]
 }
 
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name                = aws_eks_cluster.service.name
+  addon_name                  = "eks-pod-identity-agent"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [aws_eks_node_group.service]
+}
+
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  cluster_name    = aws_eks_cluster.service.name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi,
+  ]
+}
+
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name                = aws_eks_cluster.service.name
   addon_name                  = "aws-ebs-csi-driver"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
-  depends_on = [aws_eks_node_group.service]
+  depends_on = [
+    aws_eks_node_group.service,
+    aws_eks_pod_identity_association.ebs_csi,
+  ]
 }
--- vpc/ops/eks.tf
+++ vpc/ops/eks.tf
@@ -96,6 +96,33 @@ resource "aws_iam_role_policy_attachment" "eks_node_ebs_csi" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
+resource "aws_iam_role" "ebs_csi" {
+  name = "${var.eks_cluster_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "pods.eks.amazonaws.com"
+      }
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+    }]
+  })
+
+  tags = {
+    Name = "${var.eks_cluster_name}-ebs-csi-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
 resource "aws_eks_node_group" "ops" {
   cluster_name    = aws_eks_cluster.ops.name
   node_group_name = "${var.eks_cluster_name}-general"
@@ -171,7 +198,10 @@ resource "aws_eks_addon" "ebs_csi" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
-  depends_on = [aws_eks_node_group.ops]
+  depends_on = [
+    aws_eks_node_group.ops,
+    aws_eks_pod_identity_association.ebs_csi,
+  ]
 }
 
 resource "aws_eks_addon" "pod_identity_agent" {
@@ -183,3 +213,14 @@ resource "aws_eks_addon" "pod_identity_agent" {
   depends_on = [aws_eks_node_group.ops]
 }
 
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  cluster_name    = aws_eks_cluster.ops.name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi,
+  ]
+}
