a0ac0eb8ebc9481b2bd27bba81d4f01f3aea2630
JUNHO0712 <jeff070@naver.com>
2026-05-31 18:30:55 +0900

    Add files via upload

---
+++ monitoring-outputs.tf
@@ -0,0 +1,24 @@
+output "ops_aws_load_balancer_controller_role_arn" {
+  description = "IAM role ARN used by the AWS Load Balancer Controller in the Ops EKS cluster"
+  value       = module.vpc2.aws_load_balancer_controller_role_arn
+}
+
+output "ops_vpc_id" {
+  description = "VPC ID of the isolated Ops VPC"
+  value       = module.vpc2.vpc_id
+}
+
+output "ops_elasticloadbalancing_vpc_endpoint_id" {
+  description = "Interface VPC endpoint used by the Ops VPC to call the ELB API"
+  value       = module.vpc2.elasticloadbalancing_vpc_endpoint_id
+}
+
+output "ops_loki_nlb_sg_id" {
+  description = "Security group ID to insert into the Loki internal NLB Service values"
+  value       = module.vpc2.loki_nlb_sg_id
+}
+
+output "ops_thanos_receive_nlb_sg_id" {
+  description = "Security group ID to insert into the Thanos Receive internal NLB Service values"
+  value       = module.vpc2.thanos_receive_nlb_sg_id
+}
