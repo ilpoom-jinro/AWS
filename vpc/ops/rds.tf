53e4af91128fd9b44b3077060444bdc7cddccbfb
JUNHO0712 <jeff070@naver.com>
2026-06-06 16:08:22 +0900

    Update rds.tf

--- vpc/ops/rds.tf
+++ vpc/ops/rds.tf
@@ -13,7 +13,7 @@ resource "random_password" "rds" {
 resource "aws_secretsmanager_secret" "rds_password" {
   name                    = "financial-ops-rds-password"
   description             = "RDS master password for financial-ops PostgreSQL"
-  recovery_window_in_days = 7
+  recovery_window_in_days = 0
 
   tags = {
     Name = "financial-ops-rds-password"
