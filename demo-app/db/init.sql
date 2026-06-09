47e64d69ab9f0fa03510bf9a165bb21125f31a9d
JUNHO0712 <jeff070@naver.com>
2026-06-09 20:29:10 +0900

    Update init.sql

--- demo-app/db/init.sql
+++ demo-app/db/init.sql
@@ -2,41 +2,47 @@ CREATE TABLE IF NOT EXISTS recommendations (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   ticker VARCHAR(20) NOT NULL,
+  current_price INTEGER NOT NULL,
   recommended_price INTEGER NOT NULL,
   recommendation_date DATE NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 INSERT INTO recommendations
-  (name, ticker, recommended_price, recommendation_date)
+  (name, ticker, current_price, recommended_price, recommendation_date)
 VALUES
   (
     'LG에너지솔루션',
     '373220',
     450000,
-    '2026-04-28'
+    500000,
+    CURRENT_DATE
   ),
   (
     'LS',
     '006260',
     320000,
-    '2026-03-19'
+    350000,
+    CURRENT_DATE
   ),
   (
     '한국금융지주',
     '071050',
     110000,
-    '2026-02-12'
+    130000,
+    CURRENT_DATE
   ),
   (
     'HD현대일렉트릭',
     '267260',
     450000,
-    '2026-01-30'
+    520000,
+    CURRENT_DATE
   ),
   (
     '삼성전자',
     '005930',
     85000,
-    '2026-01-15'
+    95000,
+    CURRENT_DATE
   );
